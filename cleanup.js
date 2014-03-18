77e3444755d8d21cdc26e2668c41a4164248662d
Himanshu Sachdeva <hsachdeva@cloudthat.in>
2014-03-18 20:54:42 +0530

    Updated to pick AWS credentials from file if it exists

--- cleanup.js
+++ cleanup.js
@@ -4,28 +4,35 @@ var eip = require('./ec2/eip.js');
 var elb = require('./ec2/elb.js');
 var AWS = require('aws-sdk');
 var prompt = require('prompt');
+var fs = require('fs');
 
-// AWS.config.loadFromPath('./aws-credentials.json');
 config = JSON.parse(JSON.stringify(config));
 var cred = {};
 
-prompt.start();
-prompt.get(['AWS_Access_Key', 'AWS_Secret_Access_Key'], function (err, result) {
-	cred['accessKeyId'] = result.AWS_Access_Key;
-	cred['secretAccessKey'] = result.AWS_Secret_Access_Key;
-	cred['region'] = "us-east-1";
-	
-	AWS.config.update(cred);
+if (fs.existsSync('./aws-credentials.json')) {
+    AWS.config.loadFromPath('./aws-credentials.json');
+    clean()
+}
+else{
+	prompt.start();
+	prompt.get(['AWS_Access_Key', 'AWS_Secret_Access_Key'], function (err, result) {
+		cred['accessKeyId'] = result.AWS_Access_Key;
+		cred['secretAccessKey'] = result.AWS_Secret_Access_Key;
+		cred['region'] = "us-east-1";
+		
+		AWS.config.update(cred);
+		clean();
+	});
+}
+
+function clean(){
 	var regions = config['regions'];
 	console.log("Starting cleanup process ...");
 	for(var region in regions){
-
 		if(regions[region]=="true"){
 			ec2.clean(AWS,region);
 			eip.clean(AWS,region);
 			elb.clean(AWS,region);
 		}
-	}
-});
-
-
+	}	
+}
