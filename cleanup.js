dd83e05bf9cef0461021979381107282fd7e1942
Himanshu Sachdeva <hsachdeva@cloudthat.in>
2014-03-18 17:38:17 +0530

    Specify AWS Credentials at command line

--- cleanup.js
+++ cleanup.js
@@ -2,21 +2,30 @@ var config = require('./config.json');
 var ec2 = require('./ec2/ec2.js');
 var eip = require('./ec2/eip.js');
 var elb = require('./ec2/elb.js');
-
 var AWS = require('aws-sdk');
-AWS.config.loadFromPath('./aws-credentials.json');
+var prompt = require('prompt');
+
+// AWS.config.loadFromPath('./aws-credentials.json');
 config = JSON.parse(JSON.stringify(config));
+var cred = {};
 
-var regions = config['regions'];
+prompt.start();
+prompt.get(['AWS_Access_Key', 'AWS_Secret_Access_Key'], function (err, result) {
+	cred['accessKeyId'] = result.AWS_Access_Key;
+	cred['secretAccessKey'] = result.AWS_Secret_Access_Key;
+	cred['region'] = "us-east-1";
+	
+	AWS.config.update(cred);
+	var regions = config['regions'];
+	console.log("Starting cleanup process ...");
+	for(var region in regions){
 
-
-for(var region in regions){
-
-	//Terminate EC2 instances
-	if(regions[region]=="true"){
-		ec2.clean(AWS,region);
-		eip.clean(AWS,region);
-		elb.clean(AWS,region);
+		if(regions[region]=="true"){
+			ec2.clean(AWS,region);
+			eip.clean(AWS,region);
+			elb.clean(AWS,region);
+		}
 	}
+});
 
-}
+
